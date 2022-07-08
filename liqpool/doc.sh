2646651cd44f8d6687ea2214c398b0d8ec889788
despiegk <>
2022-07-08 06:01:34 +0300

    init

--- liqpool/doc.sh
+++ liqpool/doc.sh
@@ -1,3 +1,7 @@
 v fmt -w liquidity
 v doc -f html -m . -readme
 
+rm -rf docs
+mv _docs docs
+
+# open https://threefoldfoundation.github.io/dao_research/liqpool/docs/liquidity.html
--- liqpool/readme.md
+++ liqpool/readme.md
@@ -1,2 +1,3 @@
 # Liquidity Pool POC
 
+documentation see https://threefoldfoundation.github.io/dao_research/liqpool/docs/liquidity.html
